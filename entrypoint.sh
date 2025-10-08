a5afc12ec354a51623c58e774c46ec33960726a7
karasevich <karasevich@gmail.com>
2025-10-08 15:26:11 +0300

    added store/fixtures/local_seed.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,9 @@ echo "--- PostgreSQL started successfully."
 echo "--- Running database migrations..."
 python manage.py migrate --noinput
 
+echo "--- Loading local application data from fixtures/local_seed.json..."
+python manage.py loaddata local_seed.json || true
+
 # 2. Create Django Superuser if it does not exist
 # We use the environment variables defined in .env
 echo "--- Creating superuser (if not exists)..."
@@ -22,4 +25,4 @@ python manage.py createsuperuser --noinput \
 
 # 3. Start the Django web server
 echo "--- Starting Django server..."
-exec python manage.py runserver 0.0.0.0:8000
+exec gunicorn django_scart.wsgi:application --bind 0.0.0.0:8000
